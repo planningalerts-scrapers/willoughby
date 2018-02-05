e21db56ce65285e050041c53afde7f6c72cef9fe
Eric Tam <eric.tam@traceyanderic.com>
2018-02-05 15:45:21 +1100

    use ruby code and XML library

---
+++ scraper.rb
@@ -0,0 +1,4 @@
+require 'scraperwiki'
+require File.dirname(__FILE__) + '/lib_icon_rest_xml/scraper'
+
+scrape_icon_rest_xml("https://eplanning.willoughby.nsw.gov.au/pages/xc.track/searchapplication.aspx", "d=last90days&k=lodgementdate&t=da01,da01a,da02a,da03,da05,da06,da07,da10,s96,cc01a,cc01b,cc03,cc04,cd01a,cd01b,cd02,cd04,bcertu,bcertr,bcertc,tvpa,tvpa%202,tvpa%20r&o=xml")
