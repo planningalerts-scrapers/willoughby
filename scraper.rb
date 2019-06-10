535074a37715563d79c185230f1d80185e1820c2
Matthew Landauer <mlandauer@gmail.com>
2019-06-10 16:22:22 +1000

    Use latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,3 @@
 require "icon_scraper"
 
-IconScraper.scrape_with_params(
-  url: "https://eplanning.willoughby.nsw.gov.au/pages/xc.track",
-  period: "last90days",
-  types: [
-    "da01", "da01a", "da02a", "da03", "da05", "da06", "da07",
-    "da10", "s96", "cc01a", "cc01b", "cc03", "cc04", "cd01a",
-    "cd01b", "cd02", "cd04", "bcertu", "bcertr", "bcertc",
-    "tvpa", "tvpa 2", "tvpa r"
-  ]
-) do |record|
-  IconScraper.save(record)
-end
+IconScraper.scrape_and_save(:willoughby)
