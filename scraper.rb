9683606ae8069d4cca9ddca397007ea5ffc6387c
Matthew Landauer <mlandauer@gmail.com>
2019-06-10 16:18:36 +1000

    Use latest version of gem

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,15 @@
 require "icon_scraper"
 
-IconScraper.rest_xml("https://eplanning.willoughby.nsw.gov.au/pages/xc.track/searchapplication.aspx", "d=last90days&k=lodgementdate&t=da01,da01a,da02a,da03,da05,da06,da07,da10,s96,cc01a,cc01b,cc03,cc04,cd01a,cd01b,cd02,cd04,bcertu,bcertr,bcertc,tvpa,tvpa%202,tvpa%20r&o=xml")
+IconScraper.scrape_with_params(
+  url: "https://eplanning.willoughby.nsw.gov.au/pages/xc.track",
+  period: "last90days",
+  types: [
+    "da01", "da01a", "da02a", "da03", "da05", "da06", "da07",
+    "da10", "s96", "cc01a", "cc01b", "cc03", "cc04", "cd01a",
+    "cd01b", "cd02", "cd04", "bcertu", "bcertr", "bcertc",
+    "tvpa", "tvpa 2", "tvpa r"
+  ]
+) do |record|
+  record["info_url"] = record["info_url"].downcase
+  IconScraper.save(record)
+end
